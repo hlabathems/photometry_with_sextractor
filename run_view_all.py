9d0310955ef5f4c0f0777bf9dd50c4c57e62269e
Michael <hlaba@webmail.co.za>
2017-11-10 14:08:52 +0200

    Update run_view_all.py

--- run_view_all.py
+++ run_view_all.py
@@ -1,12 +1,25 @@
+"""
+This will plot all the comparison stars on one plot for inspection
+"""
 import sys, os
 import matplotlib.pyplot as plt
 from astropy.io import ascii
 
 def extract_columns(filename):
+    """
+    Parameters
+    ----------
+    filename: ascii
+    
+    Returns
+    -------
+    data: array-like object
+    """
     data = ascii.read(filename)
     
     return data
 
+# loop through files
 for filename in sys.argv[1:]:
     data = extract_columns(filename)
     
@@ -15,6 +28,7 @@ for filename in sys.argv[1:]:
     
     fname = os.path.basename(filename)
     
+    # see how many data points each have
     print(len(hjd), fname)
 
     plt.plot(hjd - 2450000, flux, '.', label = '%s' % (fname))
