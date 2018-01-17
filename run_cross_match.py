d2cc9f67fb94433e922120d3fe565af8f18a20ce
Michael <hlaba@webmail.co.za>
2018-01-17 12:56:44 +0200

    Update run_cross_match.py

--- run_cross_match.py
+++ run_cross_match.py
@@ -51,7 +51,7 @@ def cross_match(args):
     
     # headers to write to a new file
     fo = open(args.fout, 'w')
-    fo.write('CAT NUMBER HJD FLUX FLUXERR M MERR S/N ALPHA_J2000 DELTA_J2000 OBS\n')
+    fo.write('CAT NUMBER HJD FLUX FLUXERR M MERR S/N ALPHA_J2000 DELTA_J2000 OBS FLAG\n')
     fo.close()
     
     counter = 0
@@ -73,7 +73,6 @@ def cross_match(args):
             
             # use the index to retrieve hjd, observatory, exposure time
             observatory = ref['SITEID'][pos]
-            exptime = ref['EXPTIME'][pos]
             hjd = ref['HJD'][pos]
             
             # change from site names to observatory names, taken from lco website
@@ -101,20 +100,20 @@ def cross_match(args):
             # degree of accuracy
             tolerance = 4e-4
 
-            # flag bad data and do not accept negative flux values
-            if d.value <= tolerance and data['FLAGS'][idx] == 0 and data['FLUX_APER'][idx] > 0:
+            # check against tolerance
+            if d.value <= tolerance:
 
-                # convert to instrumental magnitude
-                m = -2.5 * np.log10(data['FLUX_APER'][idx] / exptime)
+                # magnitude
+                m = data['MAG_APER'][idx]
                 
-                # associated instrumental error
-                merr = 1.0857 * (data['FLUXERR_APER'][idx] / data['FLUX_APER'][idx])
+                # associated error
+                merr = data['MAGERR_APER'][idx]
                 
                 signal_to_noise = data['FLUX_APER'][idx] / data['FLUXERR_APER'][idx]
 
                 # save
                 with open(args.fout, 'a+') as outfile:
-                    outfile.write('%s.cat %s %s %s %s %s %s %s %s %s %s\n' % (without_ext, data['NUMBER'][idx], hjd[0], data['FLUX_APER'][idx], data['FLUXERR_APER'][idx], m[0], merr, signal_to_noise, data['ALPHA_J2000'][idx], data['DELTA_J2000'][idx], observatory))
+                    outfile.write('%s.cat %s %s %s %s %s %s %s %s %s %s %s\n' % (without_ext, data['NUMBER'][idx], hjd[0], data['FLUX_APER'][idx], data['FLUXERR_APER'][idx], m, merr, signal_to_noise, data['ALPHA_J2000'][idx], data['DELTA_J2000'][idx], observatory, data['FLAGS'][idx]))
                         
                 counter += 1
                     
