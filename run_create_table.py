a9942935c9109bef5f3bb699716610f242a6dbd6
Michael <hlaba@webmail.co.za>
2018-03-28 19:20:07 +0200

    Update run_create_table.py

--- run_create_table.py
+++ run_create_table.py
@@ -3,26 +3,11 @@ from astropy.io import ascii
 import argparse
 
 def create_table(args):
-    """
-    Parameters
-    ----------
-    args: array-like object 
-            corresponds to arguments passed by the user
+    ic = ImageFileCollection(location = args.path, keywords = ['HJD', 'FILTER', 'AIRMASS', 'EXPTIME', 'GAIN', 'SATURATE', 'L1FWHM', 'SECPIX', 'L1ZP', 'L1ZPERR', 'SITEID'])
     
-    Returns
-    -------
-    filename: ascii
-    
-    See Also
-    --------
-    ccdproc: offers a table summarizing values of keywords in the FITS headers of the files
-    """
-    # You can remove or add FITS headers to suit your needs. These are what you'll probably need.
-    ic = ImageFileCollection(location = args.path, keywords = ['HJD', 'FILTER', 'AIRMASS', 'EXPTIME', 'GAIN', 'SATURATE', 'L1ZP', 'L1ZPERR', 'SITEID'])
-    
-    data = [ic.summary['file'], ic.summary['HJD'], ic.summary['FILTER'], ic.summary['AIRMASS'], ic.summary['EXPTIME'], ic.summary['GAIN'], ic.summary['SATURATE'], ic.summary['L1ZP'], ic.summary['L1ZPERR'], ic.summary['SITEID']]
+    data = [ic.summary['file'], ic.summary['HJD'], ic.summary['FILTER'], ic.summary['AIRMASS'], ic.summary['EXPTIME'], ic.summary['GAIN'], ic.summary['SATURATE'], ic.summary['L1FWHM'], ic.summary['SECPIX'], ic.summary['L1ZP'], ic.summary['L1ZPERR'], ic.summary['SITEID']]
 
-    ascii.write(data, args.filename, names = ['NAME', 'HJD', 'FILTER', 'AIRMASS', 'EXPTIME', 'GAIN', 'SATURATE', 'L1ZP', 'L1ZPERR', 'SITEID'], overwrite = True)
+    ascii.write(data, args.filename, names = ['NAME', 'HJD', 'FILTER', 'AIRMASS', 'EXPTIME', 'GAIN', 'SATURATE', 'L1FWHM', 'SECPIX', 'L1ZP', 'L1ZPERR', 'SITEID'], overwrite = True)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'This program will create a table containing raw images sorted according to type of filter, airmass etc')
